edf7d71bccf72dd004f440fa7e7416edc8001d5d
MirabelleBenou <Mirabellebenou@gmail.com>
2018-01-26 14:15:45 +0100

    Ameliorations
    RSS, Deezer et explorer

---
+++ modules/deezer/deezer.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '<div class="deezer_item">';
+	echo 'Playlist Deezer';
+	echo '<div class="deezer_itemimg">';
+		echo '<a href="./modules/deezer/index.html"><img src="./modules/deezer/Logo_Deezer.png" width="auto" height="75"></a>';
+	echo '</div>';
+echo '</div>';
+	
+?>
+
+
+<div class="deezer_item">
